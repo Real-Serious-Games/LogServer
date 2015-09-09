3acc8b27ab5ffe591d63865dfeb3dce69ed19155
Ashley Davis <ashley.davis@realseriousgames.com>
2015-09-09 14:59:48 +1000

    Refactor so that the log server can be run either from the command line or required by another script.

--- server.js
+++ server.js
@@ -1,61 +1,84 @@
+'use strict';
 
-var argv = require('yargs').argv;
-var E = require('linq');
-var moment = require('moment');
+var startServer = function () {
 
-var express = require('express');
-var app = express();
+	var argv = require('yargs').argv;
+	var E = require('linq');
+	var moment = require('moment');
 
-var outputPlugin = require('./mongodb-output')({});
+	var express = require('express');
+	var app = express();
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json()); 
+	var outputPlugin = require('./mongodb-output')({});
 
-app.get("/", function (req, res) {
-	res.send("Hello");
-});
+	var bodyParser = require('body-parser')
+	app.use(bodyParser.json()); 
+
+	app.get("/", function (req, res) {
+		res.send("Hello");
+	});
+
+	//
+	// Preprocess log to our expected structure.
+	//
+	var transformLog = function (log) {
+		return {
+			Timestamp: moment(log.Timestamp).toDate(),
+			Level: log.Level,
+			MessageTemplate: log.MessageTemplate,
+			RenderedMessage: log.RenderedMessage,
+			Properties: E.from(Object.keys(log.Properties))
+				.toObject(
+					function (propertyName) {
+						return propertyName;
+					},
+					function (propertyName) {
+						return log.Properties[propertyName].Value;
+					}
+				),
+		};	
+	}
+
+	app.post('/log', function (req, res) {
+		if (!req.body) {
+			throw new Error("Expected 'body'");
+		}
+
+		if (!req.body.Logs) {
+			throw new Error("Expected 'Logs' property on body");
+		}
+
+		var logs = E.from(req.body.Logs)
+			.select(transformLog)
+			.toArray();
+
+		outputPlugin.emit(logs);
+
+		res.status(200).end();
+	});
+
+	var server = app.listen(argv.port || 3000, "0.0.0.0", function () {
+		var host = server.address().address;
+		var port = server.address().port;
+		console.log("Receiving logs at " + host + ":" + port + "/log");
+	});
+};
 
 //
-// Preprocess log to our expected structure.
-//
-var transformLog = function (log) {
-	return {
-		Timestamp: moment(log.Timestamp).toDate(),
-		Level: log.Level,
-		MessageTemplate: log.MessageTemplate,
-		RenderedMessage: log.RenderedMessage,
-		Properties: E.from(Object.keys(log.Properties))
-			.toObject(
-				function (propertyName) {
-					return propertyName;
-				},
-				function (propertyName) {
-					return log.Properties[propertyName].Value;
-				}
-			),
-	};	
+// http://stackoverflow.com/a/6398335/25868
+// 
+if (require.main === module) { 
+	
+	console.log('Starting from command line.');
+
+	//
+	// Run from command line.
+	//
+	startServer();
 }
-
-app.post('/log', function (req, res) {
-	if (!req.body) {
-		throw new Error("Expected 'body'");
-	}
-
-	if (!req.body.Logs) {
-		throw new Error("Expected 'Logs' property on body");
-	}
-
-	var logs = E.from(req.body.Logs)
-		.select(transformLog)
-		.toArray();
-
-	outputPlugin.emit(logs);
-
-	res.status(200).end();
-});
-
-var server = app.listen(argv.port || 3000, "0.0.0.0", function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log("Receiving logs at " + host + ":" + port + "/log");
-});
+else {
+	// 
+	// Required from another module.
+	//
+	module.exports = startServer;
+}
